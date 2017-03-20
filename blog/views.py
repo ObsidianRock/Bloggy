914822c9c0cf3eccdb395039d2506d4ba09d3897
Obsidian <ObsidianRock@users.noreply.github.com>
2017-03-20 21:50:35 +0000

    added pagination blog post page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from django.core.paginator import Paginator, EmptyPage,\
+ PageNotAnInteger
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    all_posts = Post.published.all()
+    paginator = Paginator(all_posts, 3)
+
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
 
 
 def post_detail(request, year, month, day, post):
